807e699d8ea2c92703afebec10a596a3838f22f9
hui xie <huixie01@users.noreply.github.com>
2019-09-24 15:24:49 -0400

    Add files via upload

--- Geolocation.java
+++ Geolocation.java
@@ -1,14 +1,17 @@
 package xmRadioLibrary;
 
 public class Geolocation implements Location {
-	public float lon;
-	public float lat;
+	public String lon;
+	public String lat;
 	public String addr;
 	public String type="geo";
-	public Geolocation(float lon, float lat, String addr) {
+	public Geolocation(String lon, String lat, String addr) {
 		this.lon=lon;
 		this.lat=lat;
 		this.addr=addr;
+	}
+	public String toString() {
+		return lon + ", " + lat + ", " + addr;
 	}
 
 }
