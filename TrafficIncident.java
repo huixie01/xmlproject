8ca98bf29d100fbe323d4805f8cc8ff28b4fbbf9
hui xie <huixie01@users.noreply.github.com>
2019-09-24 14:41:51 -0400

    Add files via upload

--- TrafficIncident.java
+++ TrafficIncident.java
@@ -4,16 +4,17 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TrafficIncident {
-	public int id;
-	public int ecode;
-	public int se;
+	public String id;
+	public String ecode;
+	public String se;
 	public Location loc;
 	public LocalDateTime startTime;
 	public LocalDateTime endTime;
-	DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-dd-MMTHH:mm:ssZ");
+	DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-dd-MMEHH:mm:ssZ");
+	
 	public String desc;
 	public int delay;
-	public TrafficIncident(int id, int ecode, int se) {
+	public TrafficIncident(String id, String ecode, String se) {
 		this.id=id;
 		this.ecode=ecode;
 		this.se=se;
