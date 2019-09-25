1ece07f69cc1c580cc5945851114f77777957674
hui xie <huixie01@users.noreply.github.com>
2019-09-25 13:41:16 -0400

    Update TrafficIncident.java

--- TrafficIncident.java
+++ TrafficIncident.java
@@ -1,34 +1,70 @@
 package xmRadioLibrary;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
-public class TrafficIncident {
+import java.time.Instant;
+
+public class TrafficIncident<T> {
 	public String id;
 	public String ecode;
 	public String se;
-	public Location loc;
+	private T t;
+	
+	
 	public LocalDateTime startTime;
 	public LocalDateTime endTime;
-	DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-dd-MMEHH:mm:ssZ");
+	
+	
 	
 	public String desc;
 	public int delay;
+	
 	public TrafficIncident(String id, String ecode, String se) {
 		this.id=id;
 		this.ecode=ecode;
 		this.se=se;
-		this.loc=null;
+		this.t=null;
+		
+		
 	}
-	public void setLocation(Location loc) {
-		this.loc = loc;
-	}
+	public void set(T t) { this.t = t; }
+    public T get() { return t; }
+	
+    public void setDesc(String text) {
+    	this.desc = text;
+    }
+	
+    public void setStartTime(String startTm) {
+    	Instant instant = Instant.parse(startTm);
+    	this.startTime = LocalDateTime.ofInstant(instant, ZoneId.of(ZoneOffset.UTC.getId()));
+    }
+    
+    public void setEndTime(String endTm) {
+    	Instant instant = Instant.parse(endTm);
+    	this.endTime = LocalDateTime.ofInstant(instant,ZoneId.of(ZoneOffset.UTC.getId()));
+    }
 	public void getExtraElements() {
 		System.out.println("startTime: " + startTime.toString());
 		System.out.println("endTime: " + endTime.toString());
 		System.out.println("desc: " + desc);
 	}
-	public Location getLocation() {
-		return loc;
+	
+	public String toString() {
+		String obj_str = "id: " + id;
+		obj_str += "\n ecode: " + ecode;
+		obj_str += "\n se: " + se;
+		if (t != null)
+			obj_str += "\n loc: " + t.toString();
+		if (startTime != null)
+			obj_str += "\n startTime: " + startTime.toString();
+		if (endTime != null)
+			obj_str += "\n endTime: " + endTime.toString();
+		if (desc != null)
+			obj_str += "\n desc: " + desc;
+		return obj_str;
 	}
 }
+
+
