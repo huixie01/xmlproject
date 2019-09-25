d02691a80bf551b5ce6862876fd7ed655090e63e
hui xie <huixie01@users.noreply.github.com>
2019-09-25 13:39:33 -0400

    Update TmcLocation.java

--- TmcLocation.java
+++ TmcLocation.java
@@ -1,18 +1,31 @@
 package xmRadioLibrary;
 
 
-public class TmcLocation implements Location {
-	public String startId="10000";
-	public char dir='+';
-	public String startExt="0";
+public class TmcLocation extends Location {
+	public String startId;
+	public char dir;
+	public String startExt;
 	public String type="tmc";
 	public TmcLocation(String startId, char dir, String startExt){
 		this.startId = startId;
 		this.dir = dir;
 		this.startExt = startExt;		
 	}
+	public String getType() {
+		return this.type;
+	}
 	public String toString() {
 		return startId + ", " + dir + ", " + startExt;
 	}
+	public void setStartId(String startId) {
+		this.startId = startId;
+	}
+	public void setDir(char dir) {
+		this.dir = dir;
+	}
+	public void setStartExt(String startExt) {
+		this.startExt = startExt;
+	}
+	
 	
 }
