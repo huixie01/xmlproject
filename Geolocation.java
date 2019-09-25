37dd03a23b2aacbaadb1e1b6dfef36c730302fab
hui xie <huixie01@users.noreply.github.com>
2019-09-25 13:39:05 -0400

    Update Geolocation.java

--- Geolocation.java
+++ Geolocation.java
@@ -1,17 +1,35 @@
 package xmRadioLibrary;
 
-public class Geolocation implements Location {
-	public String lon;
-	public String lat;
-	public String addr;
+public class Geolocation extends Location {
+	private String lon;
+	private String lat;
+	private String addr;
+	
 	public String type="geo";
 	public Geolocation(String lon, String lat, String addr) {
 		this.lon=lon;
 		this.lat=lat;
 		this.addr=addr;
 	}
+	public String getType() {
+		return this.type;
+	}
 	public String toString() {
 		return lon + ", " + lat + ", " + addr;
 	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getAddr() {
+		return this.addr;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	
+	
 
 }
